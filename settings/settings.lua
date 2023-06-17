b777a006b11c7259f8922c695d3f6f621b4c1735
Anthony Oakley <agoakley@gmail.com>
2023-06-17 22:07:06 +0100

    mushrooms and typo

--- settings/settings.lua
+++ settings/settings.lua
@@ -80,7 +80,7 @@ local panel = {
     name = "FOB - Companion Helper",
     displayName = "|cdc143cFOB|r - Companion Helper",
     author = "Flat Badger",
-    version = "2.10.6",
+    version = "2.10.7",
     slashCommand = "/fob",
     registerForRefresh = true
 }
